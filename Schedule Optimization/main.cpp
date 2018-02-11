7e6dd918fc72e486d9e62dba8765173be3570775
Diego Brown <dsb4k8@gmail.com>
2018-02-10 23:25:49 -0600

    Initial Commit

---
+++ Schedule Optimization/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Schedule Optimization
+//
+//  Created by Diego Brown on 2/10/18.
+//  Copyright © 2018 Diego Brown. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
